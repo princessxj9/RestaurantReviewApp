9cedfed708ccd8dc2bcd99f55a8f65f6b7554031
princessxj9 <johnsonprincess25@gmail.com>
2022-05-25 20:06:26 -0400

    commit

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine() )
+
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('new app tings')
+    res.render('home')
 })
 
+app.get('/', (req, res) => {
+   
+    res.render('places/index', {places})
+  })
+  
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
 })
 
 app.listen(process.env.PORT)
