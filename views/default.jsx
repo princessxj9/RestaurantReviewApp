091b7de2a1908733187dc0da933cae4e28d86682
princessxj9 <johnsonprincess25@gmail.com>
2022-06-04 11:08:08 -0400

    route connected

--- views/default.jsx
+++ views/default.jsx
@@ -9,6 +9,19 @@ function Def(html) {
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href='/'>Home</a>
+                        </li>
+                        <li>
+                            <a href='/places'>Places</a>
+                        </li>
+                        <li>
+                            <a href='/places/new'>Add Places</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
         </html>
