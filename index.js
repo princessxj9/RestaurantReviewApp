f4785776626c0406628bdcdca83679bd3e1f899e
princessxj9 <johnsonprincess25@gmail.com>
2022-06-06 17:30:11 -0400

    FIXED ERR THANG

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+// Modules and Globals
 require('dotenv').config()
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express();
 
-//Express Settings 
-app.set('veiws', __dirname + '/views')
+// Express Settings
 app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine() )
+app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
 
 //controllers & routes 
 app.use('/places', require('./controllers/places'))
@@ -15,10 +18,6 @@ app.use('/places', require('./controllers/places'))
 app.get('/', (req, res) => {
     res.render('home')
 })
-
-app.get('/', (req, res) => {
-    res.render('places/index', {places})
-  })
   
 app.get('*', (req, res) => {
     res.render('error404')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,14 +1,57 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
-    return (
-        <Def>
-          <main>
-            <h1>Show Page</h1>
-          </main>
-        </Def>
-    )
+function show(data) {
+  return (
+    <Def>
+      <main>
+        <h1>{data.place.name}</h1>
+        <div className="container">
+
+          <div className="rowA">
+
+            <div className="col-sm-8">
+            <img src={data.place.pic} alt={data.place.name}/>
+                <p className='text-center'>
+                    Located in {data.place.city}, {data.place.state}
+                </p>
+            </div>
+
+            <div className="col-sm-4">
+             place/location
+            </div>
+
+          </div>
+
+          <div className="rowB">
+
+            <div className="col-sm-3">
+            rating
+            </div>
+
+            <div className="col-sm-3">
+             rave
+            </div>
+
+            <div className="col-sm-3">
+             rant
+            </div>
+
+          </div>
+
+        </div>
+        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+          Edit
+        </a>
+        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+          <button type="submit" className="btn btn-danger">
+            Delete
+          </button>
+        </form>
+      </main>
+    </Def>
+  )
 }
+
 
 module.exports = show
